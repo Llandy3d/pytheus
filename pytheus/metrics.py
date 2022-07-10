c0b04e038759b47140a12aec61e706d5b017484a
Llandy Riveron Del Risco <llandy3d@gmail.com>
2022-07-10 16:00:33 +0200

    more playing

--- pytheus/metrics.py
+++ pytheus/metrics.py
@@ -2,19 +2,38 @@ from dataclasses import dataclass
 from typing import Sequence
 
 
+Labels = dict[str, str]
+
+
 class MetricCollector:
 
-    def __init__(self, name: str, required_labels: Sequence[str] | None) -> None:
+    def __init__(self, name: str, required_labels: Sequence[str] | None = None) -> None:
         self.name = name
         self._required_labels = set(required_labels) if required_labels else None
+        # TODO: pass metric class
         self._metric = Metric(self)
         self._labeled_metrics: dict[tuple[str, str], Metric] = {}
 
+        # this will register to the collector
 
+
+# class or function based creation ?
 class Metric:
 
-    def __init__(self, collector: MetricCollector) -> None:
+    def __init__(
+        self,
+        collector: MetricCollector,
+        labels: Labels | None = None,
+    ) -> None:
         self._collector = collector
+        self._labels = labels
+        self._can_observe = False  # depending on labels, etc..
+
+
+# this could be a class method, but might want to avoid it
+def create_metric(name: str, required_labels: Sequence[str] | None = None) -> Metric:
+    collector = MetricCollector(name, required_labels)
+    return Metric(collector)
 
 
 @dataclass
@@ -24,10 +43,12 @@ class Sample:
     value: float
 
 
+# maybe just go with the typing alias
 @dataclass
 class Label:
     name: str
     value: str
 
 
-m = MetricCollector('name', ['bbo'])
+# m = MetricCollector('name', ['bbo'])
+my_metric = create_metric('name', ['bbo'])
