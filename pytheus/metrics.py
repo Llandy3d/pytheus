b50cb8879a4edb9040f15c42d55f0d3cfde59d55
Llandy Riveron Del Risco <llandy3d@gmail.com>
2022-07-10 03:41:54 +0200

    stub

---
+++ pytheus/metrics.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Sequence
+
+
+class MetricCollector:
+
+    def __init__(self, name: str, required_labels: Sequence[str] | None) -> None:
+        self.name = name
+        self._required_labels = set(required_labels) if required_labels else None
+        self._metric = Metric(self)
+        self._labeled_metrics: dict[tuple[str, str], Metric] = {}
+
+
+class Metric:
+
+    def __init__(self, parent: MetricCollector) -> None:
+        self.parent = parent
+
+
+@dataclass
+class Sample:
+    name: str
+    labels: dict[str, str]
+    value: float
+
+
+@dataclass
+class Label:
+    name: str
+    value: str
+
+
+m = MetricCollector('name', ['bbo'])
