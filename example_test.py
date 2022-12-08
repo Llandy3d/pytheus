9e9aab1a54c16c7d4f184607b35a93a69cb9b751
Llandy Riveron Del Risco <llandy3d@gmail.com>
2022-12-08 22:34:37 +0100

    redis backend tests

--- example_test.py
+++ example_test.py
@@ -3,11 +3,24 @@ from flask import Flask
 from pytheus.metrics import Counter, Histogram
 from pytheus.exposition import generate_metrics
 
+from pytheus.backends import load_backend
+from pytheus.backends.redis import MultipleProcessRedisBackend
+
+
+load_backend(
+    backend_class=MultipleProcessRedisBackend,
+    backend_config={
+      "host": "127.0.0.1",
+      "port":  6379
+    },
+)
+
 
 app = Flask(__name__)
 
 
 histogram = Histogram('page_visits_latency_seconds', 'used for testing')
+histogram_labeled = Histogram('page_visits_latency_seconds_labeled', 'used for testing', required_labels=['speed'], default_labels={'speed': 'normal'})
 # counter = Counter('page_visits_total', 'total number of page visits')
 
 
@@ -19,14 +32,16 @@ def metrics():
 @app.route('/')
 def home():
     with histogram.time():
-        return 'hello world!'
+        with histogram_labeled.time():
+            return 'hello world!'
 
 
 @app.route('/slow')
 def slow():
     with histogram.time():
-        time.sleep(3)
-        return 'hello world! from slow!'
+        with histogram_labeled.labels({'speed': 'slow'}).time():
+            time.sleep(3)
+            return 'hello world! from slow!'
 
 
 app.run(host='0.0.0.0', port=8080)
