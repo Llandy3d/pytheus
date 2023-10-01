c80fc9a02af6e75ff5d35519283ec58aff5f894e
Llandy Riveron Del Risco <llandy3d@gmail.com>
2023-10-01 22:17:33 +0200

    Revert "collector registry adapter"
    This reverts commit d5d7c5b32942a7256708ba89e2cdb49ef9e9e7a0.

--- pytheus/experimental/compatibility.py
+++ pytheus/experimental/compatibility.py
@@ -8,20 +8,22 @@ I just need to try some things without changing a whole internal library first :
 3. ?
 """
 from pytheus.experimental.adapters import (
-    CollectorRegistryAdapter,
     CounterAdapter,
     GaugeAdapter,
     HistogramAdapter,
     SummaryAdapter,
 )
 from pytheus.exposition import generate_metrics
+from pytheus.registry import REGISTRY, CollectorRegistry
 
-default_global_registry = CollectorRegistryAdapter()
+# CounterMetricFamily ???
+
+# exposition.MetricsHandler needs patching on .registry ??
 
 patches = {
     # registry
-    "REGISTRY": default_global_registry,
-    "CollectorRegistry": CollectorRegistryAdapter,
+    "REGISTRY": REGISTRY,
+    "CollectorRegistry": CollectorRegistry,
     # metrics
     "Counter": CounterAdapter,
     "Gauge": GaugeAdapter,
