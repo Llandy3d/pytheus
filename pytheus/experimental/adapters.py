2f679c8790b7f34d4449a2aeab6abc31d575713c
Llandy Riveron Del Risco <llandy3d@gmail.com>
2023-10-01 18:43:59 +0200

    use client DEFAULT_BUCKETS

--- pytheus/experimental/adapters.py
+++ pytheus/experimental/adapters.py
@@ -82,6 +82,24 @@ class DecoratorContextManagerAdapter:
 
 
 class HistogramAdapter:
+    DEFAULT_BUCKETS = (
+        0.005,
+        0.01,
+        0.025,
+        0.05,
+        0.075,
+        0.1,
+        0.25,
+        0.5,
+        0.75,
+        1.0,
+        2.5,
+        5.0,
+        7.5,
+        10.0,
+        float("inf"),
+    )
+
     def __init__(
         self,
         name: str,
